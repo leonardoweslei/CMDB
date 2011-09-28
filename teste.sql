a30393f2f586770706e969c3fe9425c5a8b9feed
Leonardo Weslei Diniz <leonardoweslei@gmail.com>
2011-09-28 08:57:41 -0300

    SQL do exemplo

---
+++ teste.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `teste` (
+  `codigo` int(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `sobrenome` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
