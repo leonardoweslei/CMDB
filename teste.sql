793f49d39e21a61eff907dd4343c0c56d94d5c5a
Leonardo Weslei Diniz <leonardoweslei@gmail.com>
2011-11-29 12:06:34 -0200

    Atualizando sql do exemplo

--- teste.sql
+++ teste.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `teste` (
+CREATE TABLE IF NOT EXISTS `teste` (
   `codigo` int(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `dependencia` int(5) unsigned zerofill DEFAULT NULL,
   `nome` varchar(20) NOT NULL,
   `sobrenome` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
+  PRIMARY KEY (`codigo`),
+  KEY `dependencia` (`dependencia`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+ALTER TABLE `teste` ADD CONSTRAINT `teste_ibfk_1` FOREIGN KEY (`dependencia`) REFERENCES `teste` (`codigo`) ON DELETE SET NULL ON UPDATE CASCADE;
