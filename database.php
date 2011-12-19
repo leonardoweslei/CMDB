31ac024fb8f988866d7263179e87f3705e090491
Leonardo Weslei Diniz <leonardoweslei@gmail.com>
2011-12-19 16:02:59 -0200

    Melhorando classe para que aceite atribuição de configurações definidas em um array global chamado "database_params" ou através de parametros recebidos tratamento do metodo getPDO para retornar um objeto PDO ou um objeto extendido do PDO de acordo com o driver usado

--- database.php
+++ database.php
@@ -11,39 +11,83 @@
      */
 class database
 {
+	private $dsn;
+	private $user;
+	private $passwd;
+	private $host;
+	private $dbname;
 	private $conection=false;
-	function __construct()
+	// $dsn="mysql", $host="localhost", $user="root", $passwd=false, $dbname=false 
+	function database()
 	{
-		$this->conect();
+		$params=array();
+		if(func_num_args()==0)
+		{
+			global $database_params;
+			$params=$database_params;
+		}
+		else
+		{
+			$args=func_get_args();
+			$params=array(
+				'dsn'=>isset($args[0])?$args[0]:"mysql",
+				'host'=>isset($args[1])?$args[1]:"localhost",
+				'user'=>isset($args[2])?$args[2]:"root",
+				'passwd'=>isset($args[3])?$args[3]:"",
+				'dbname'=>isset($args[4])?$args[4]:false
+			);
+		}
+		$this->set_attr($params);
 	}
-	private function conect()
+	private function set_attr($values)
 	{
-		global $bd_data;
-		$this->conection=self::getPDO();
+		foreach ($values as $k=>$v)
+		{
+			$this->$k=$v;
+		}
 	}
 	public function getconection()
 	{
 		if(empty($this->conection))
 		{
-			$this->conect();
+			$this->conection=$this->getPDO();
 		}
 		return $this->conection;
 	}
 	public function getPDO()
 	{
-		global $bd_data;
-		return new PDO
-		(
-			$bd_data['dsn'].
-			":host=".$bd_data['host'].
+		if(is_file("dsn/{$this->dsn}.php"))
+		{
+			require_once("dsn/{$this->dsn}.php");
+			$dsn="database_{$this->dsn}";
+			return new $dsn
 			(
-				(isset($bd_data['dbname'])&&!empty($bd_data['dbname']))?
-				";dbname=".$bd_data['dbname']:
-				''
+			$this->dsn.
+			":host=".$this->host.
+			(
+			(isset($this->dbname)&&!empty($this->dbname))?
+			";dbname=".$this->dbname:
+								''
 			),
-			$bd_data['user'],
-			$bd_data['passwd']
-		);
+			$this->user,
+			$this->passwd
+			);
+		}
+		else
+		{
+			return new PDO
+			(
+				$this->dsn.
+				":host=".$this->host.
+				(
+					(isset($this->dbname)&&!empty($this->dbname))?
+					";dbname=".$this->dbname:
+					''
+				),
+				$this->user,
+				$this->passwd
+			);
+		}
 	}
 }
 ?>
